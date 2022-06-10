4989143b5380fa718dc87f546f679b51baa35e6f
WYJTang <643392718@qq.com>
2022-06-10 12:09:20 +0800

    删除静态页面

--- source/teams/hwxm/js/load.js
+++
@@ -1,4 +0,0 @@
-$(".head").load("data/data.html .box_head");
-$(".nav").load("data/data.html .box_nav");
-$(".footer").load("data/data.html .box_footer");
-$(".footer_foot").load("data/data.html .box_footer_foot");
